4086a5e08a610518c5aae3d9b8ee8d34e8fae029
Che <chethan@mocheco.com>
2024-05-09 11:31:35 +0530

    chore(test): Add build_runner dependency for code generation

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:minm/main.dart';
 import 'package:mockito/mockito.dart';
-
-class MockBarcodeScanner extends Mock implements BarcodeScanner {}
-
-class MockBookDetailsFetcher extends Mock implements BookDetailsFetcher {}
+import 'mocks.mocks.dart';
 
 void main() {
   late MockBarcodeScanner mockBarcodeScanner;
@@ -17,7 +14,7 @@ void main() {
 
   testWidgets('Book details test', (WidgetTester tester) async {
     when(mockBarcodeScanner.scanBarcode())
-        .thenAnswer((_) async => '1234567890');
+        .thenAnswer((_) => Future.value('1234567890'));
     when(mockBookDetailsFetcher.fetchBookDetails('1234567890'))
         .thenAnswer((_) async => 'Mock Book Details');
 
@@ -39,3 +36,45 @@ void main() {
     expect(find.text('Mock Book Details'), findsOneWidget);
   });
 }
+
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:minm/main.dart';
+// import 'package:mockito/mockito.dart';
+
+// class MockBarcodeScanner extends Mock implements BarcodeScanner {}
+
+// class MockBookDetailsFetcher extends Mock implements BookDetailsFetcher {}
+
+// void main() {
+//   late MockBarcodeScanner mockBarcodeScanner;
+//   late MockBookDetailsFetcher mockBookDetailsFetcher;
+
+//   setUp(() {
+//     mockBarcodeScanner = MockBarcodeScanner();
+//     mockBookDetailsFetcher = MockBookDetailsFetcher();
+//   });
+
+//   testWidgets('Book details test', (WidgetTester tester) async {
+//     when(mockBarcodeScanner.scanBarcode())
+//         .thenAnswer((_) => Future.value('1234567890'));
+//     when(mockBookDetailsFetcher.fetchBookDetails('1234567890'))
+//         .thenAnswer((_) async => 'Mock Book Details');
+
+//     // Build our app and trigger a frame.
+//     await tester.pumpWidget(MyApp(
+//       barcodeScanner: mockBarcodeScanner,
+//       bookDetailsFetcher: mockBookDetailsFetcher,
+//     ));
+
+//     // Verify that our book details start with 'No book scanned yet'.
+//     expect(find.text('No book scanned yet'), findsOneWidget);
+
+//     // Tap the 'Scan Barcode and Fetch Details' button and trigger a frame.
+//     await tester.tap(find.text('Scan Barcode and Fetch Details'));
+//     await tester.pump();
+
+//     // Verify that our book details have been updated.
+//     expect(find.text('No book scanned yet'), findsNothing);
+//     expect(find.text('Mock Book Details'), findsOneWidget);
+//   });
+// }
